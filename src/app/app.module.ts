f8980d9f459c54c37d59fb4c4d25b6108965b719
digneudon <devtree74@gmail.com>
2021-08-03 21:01:16 +0900

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Test1Component } from './pages/test1/test1.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [AppComponent, HelloComponent, Test1Component],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/test1/test1.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.css']
+})
+export class Test1Component implements OnInit {
+
+  selectedItem: number = 0;
+  selectedItem2: number = 0;
+
+  items: { id: number; name: string; }[] = [];
+
+  // 制御用(trueが非活性)
+  isDisabledItem: boolean = true;
+
+  constructor(  ) { }
+
+  ngOnInit() {
+    // 配列設定
+    for (let index = 0; index < 10; index++) {
+      this.items.push({ id: index + 1, name: `${index + 1}ban` });
+    }
+  }
+
+  onChange() {
+    if (this.selectedItem != 0) {
+      this.isDisabledItem = false;
+    } else {
+      this.isDisabledItem = true;
+    }
+  }
+
+}
