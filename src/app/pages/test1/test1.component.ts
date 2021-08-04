90d8748303f1d5f133989f0e613e24bca5f5c392
digneudon <devtree74@gmail.com>
2021-08-05 07:42:47 +0900

    2 files modified

--- src/app/pages/test1/test1.component.ts
+++ src/app/pages/test1/test1.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test1.component.css']
 })
 export class Test1Component implements OnInit {
-  selectedItem: { id: number; name: string } = undefined;
-  selectedItem2: { id: number; name: string } = undefined;
+  selectedItem: { id: number; name: string };
+  // selectedItem2: { id: number; name: string };
+  selectedItem2: { subId: string; name: string };
 
   items: { id: number; name: string }[] = [];
+  items2: { subId: string; name: string }[] = [];
 
   // 制御用(trueが非活性)
   isDisabledItem: boolean = true;
@@ -21,6 +23,12 @@ export class Test1Component implements OnInit {
     for (let index = 0; index < 10; index++) {
       this.items.push({ id: index + 1, name: `${index + 1}ban` });
     }
+
+    this.items2.push({ subId: 'aaa', name: 'saisyo' });
+    this.items2.push({ subId: 'bbb', name: 'nikome' });
+    this.items2.push({ subId: 'ccc', name: 'sankome' });
+    this.items2.push({ subId: 'ddd', name: 'yonkome' });
+    this.items2.push({ subId: 'eee', name: 'gokome' });
   }
 
   onChange() {
